2a31853df348ba993905b7a8454bd067db45a6b8
Mariana <nanitamejes@hotmail.com>
2015-10-26 20:45:04 -0500

    Proyecto contactos

---
+++ app/src/main/java/com/example/estudiante/nomalo/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.estudiante.nomalo;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int TIME = 3000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME);
+    }
+}
